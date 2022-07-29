3794578141191049a292d46c22bcfa82a40afd64
Gagan Deep <gagan.d22p@gmail.com>
2022-07-29 23:12:23 +0530

    more css

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => {
   return (
     <div>
       <Navbar />
-      <section id="video">
+      <section className="largescreen" id="video">
         <video
           src="images/TCRbgVideo.mp4"
           id="realvideo"
@@ -18,12 +18,13 @@ const Homepage = () => {
           height="100%"
         ></video>
       </section>
+      <section className="smallscreen centerize">
+        <img src="images/TCR_blackLogo.png" alt=""></img>
+      </section>
 
       <section id="aboutUs" className="centerize">
         <div className="box">
-          <h1 className="heading">
-            About Us
-          </h1>
+          <h1 className="heading">About Us</h1>
           TEAM COMBAT ROBOTICS founded in 2018, is a team of enthusiast
           undergraduate students from different factions of engineering, working
           on building efficient combat capable robots. TCR has built the first
@@ -33,20 +34,27 @@ const Homepage = () => {
         </div>
         <div className="row">
           <div className="column box">
-            <h1 className="heading">
-              Vision
-            </h1>
+            <h1 className="heading">Vision</h1>
             To build the best bots, cause no unnecessary harm and to compete in
             various national and international robotics events held in India.
           </div>
           <div className="column box">
-            <h1 className="heading">
-              Mission
-            </h1>
+            <h1 className="heading">Mission</h1>
             To be one of the technical student projects to design,fabricate and
             manufacture innovative combat robots and at the same time focus on
             the practicality and feasibility of the bots in the combat arena.
           </div>
+        </div>
+        <div className="smallscreen box">
+          <h1 className="heading">Vision</h1>
+          To build the best bots, cause no unnecessary harm and to compete in
+          various national and international robotics events held in India.
+        </div>
+        <div className="smallscreen box">
+          <h1 className="heading">Mission</h1>
+          To be one of the technical student projects to design,fabricate and
+          manufacture innovative combat robots and at the same time focus on the
+          practicality and feasibility of the bots in the combat arena.
         </div>
       </section>
 
@@ -55,42 +63,33 @@ const Homepage = () => {
       <section
         id="contactUs"
         className="centerize"
-        style={{ color: "aliceblue", background: "#000" }}
+        style={{ background: "#000", width: "100vw" }}
       >
-        <div>
-          <h1 className="text-capitalize">Contact Us!</h1>
-          <hr className="w-10 mx-auto" />
-          <div className="parent">
-            <div className="Contacts">
-              <div className="container-fluid d-flex justify-content-start align-items-center flex-row">
-                <i
-                  className="fa fa-volume-control-phone "
-                  id="phonenumber"
-                  style={{ color: "aliceblue" }}
-                  aria-hidden="true"
-                >
-                  {" "}
-                  +9189715 68267
-                </i>
+        <div className="row">
+          <div className="Contacts contactColumn left">
+            <i
+              className="fa fa-volume-control-phone "
+              id="phonenumber"
+              style={{ color: "aliceblue" }}
+              aria-hidden="true"
+            >
+              {" "}
+              +9189715 68267
+            </i>
+            <i className="fas fa-envelope m-2"> tcr.manipal@gmail.com </i>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.6235867558083!2d74.79300428707894!3d13.351682121356601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbca4a63a6625cf%3A0x84cae5b07cb4654c!2sMIT%20Central%20Library!5e0!3m2!1sen!2sin!4v1645613015681!5m2!1sen!2sin"
+              id="map"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
 
-                <div className="vl m-2"></div>
-
-                <i className="fas fa-envelope m-2"> tcr.manipal@gmail.com</i>
-              </div>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.6235867558083!2d74.79300428707894!3d13.351682121356601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbca4a63a6625cf%3A0x84cae5b07cb4654c!2sMIT%20Central%20Library!5e0!3m2!1sen!2sin!4v1645613015681!5m2!1sen!2sin"
-                id="map"
-                allowfullscreen=""
-                loading="lazy"
-              ></iframe>
-              <h2 className="mt-4">Location -: Behind MIT Central Library</h2>
-            </div>
-            <div className="teampic">
-              <div className="col-sm-12 col-md-6 col-lg-6 ">
-                <figure>
-                  <img className="img-fluid" src="images/team.jpeg" alt="" width={100}/>
-                </figure>
-              </div>
+          <div className="teampic contactColumn right">
+            <div className="col-sm-12 col-md-6 col-lg-6 ">
+              <figure>
+                <img className="img-fluid" src="images/team.jpeg" alt="" />
+              </figure>
             </div>
           </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,72 +1,41 @@
-import React, { useState } from "react";
 import "./Navbar.css";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import React from "react";
 
 const Navbar = () => {
-  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
-    <>
-      <nav className="main-nav">
-        {/* 1st logo part  */}
-        <div className="logo">
-          <h2>
-            Team Combat Robotics
-          </h2>
-        </div>
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="name">TEAM COMBAT ROBOTICS</span>
+        </Link>
+      </div>
 
-        {/* 2nd menu part  */}
-        <div
-          className={
-            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
-          }>
-            <ul>
-
-           <li>
+      <div className="topbarRight">
+        <div className="topbarLinks">
           <HashLink to="#aboutUs" style={{ textDecoration: "none", color:"black" }}>
-            <span className="topbarLink">About</span>
+            <span className="topbarLink">About Us</span>
           </HashLink>
-          </li> 
-          <li>
           <Link to="/team" style={{ textDecoration: "none", color:"black" }}>
             <span className="topbarLink">Team</span>
           </Link>
-          </li>
-          <li>
           <Link to="/alumni" style={{ textDecoration: "none", color:"black" }}>
             <span className="topbarLink">Alumni</span>
           </Link>
-          </li>
-          <li>
           <Link to="/works" style={{ textDecoration: "none", color:"black" }}>
             <span className="topbarLink">Works</span>
           </Link>
-          </li> 
-          <li>
           <HashLink to="#sponsors" style={{ textDecoration: "none", color:"black" }}>
             <span className="topbarLink">Sponsors</span>
           </HashLink>
-          </li> 
-          <li>
           <HashLink to="#contactUs" style={{ textDecoration: "none", color:"black" }}>
             <span className="topbarLink">Contact</span>
           </HashLink>
-          </li> 
-
-          </ul>
         </div>
-        {/* hamburget menu start  */}
-        <div className="hamburger-menu">
-            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
-              <GiHamburgerMenu />
-            </a>
-          </div>
-      </nav>
-
-      
-    </>
+      </div>
+    </div>
   );
-};
+}
 
-export default Navbar;
+export default Navbar
